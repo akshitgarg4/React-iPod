02756d2eda1c0e1e8d76681bcdc90a53439aca1f
akshitgarg4 <akshit2000garg@gmail.com>
2020-06-29 13:12:10 +0530

    lower ipod styling done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+      <div className="ipod">
+        <div className="lower-body">
+          <div className="outer-circle">
+            <p style={{textAlign:"center",paddingTop:15}} >MENU</p>
+            <i class="fas fa-fast-backward" style={{display:"inline-block", paddingTop:26,paddingLeft:18}}></i>
+            <i class="fas fa-fast-forward"  style={{paddingLeft:112}}></i>
+            <p style={{textAlign:"center",paddingTop:30}}><i class="fas fa-play" ></i></p>
+            <div className="inner-circle">
+              <p style={{textAlign:"center",padding:16}}>SELECT</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
