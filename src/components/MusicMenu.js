fb4e5e2d97b84ca7d3d7516078e9cd8b83afc397
akshitgarg4 <akshit2000garg@gmail.com>
2020-06-30 14:26:28 +0530

    songs playing successfully

--- src/components/MusicMenu.js
+++ src/components/MusicMenu.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-
-
- 
 class MusicMenu extends React.Component{
         
        render()
@@ -12,7 +9,8 @@ class MusicMenu extends React.Component{
             <div className="menu" style={styles.menu}>
                 <div className="menu-items">
                     
-                    {(this.props.activeSong==='song1' && this.props.activeMenu==='music' && this.props.play==='') && <div>
+                    {(this.props.activeSong==='song1' && this.props.activeMenu==='music' && this.props.play==='') &&
+                    <div>
                     <p style={styles.para}>SONGS</p>
                     <p style={styles.active} >song1</p>
                     <p style={styles.para2}>song2</p>
@@ -21,7 +19,8 @@ class MusicMenu extends React.Component{
                     <p style={styles.para2}>Back</p>
                     </div>
                     }
-                    {(this.props.activeSong==='song2' && this.props.activeMenu==='music' && this.props.play==='') && <div>
+                    {(this.props.activeSong==='song2' && this.props.activeMenu==='music' && this.props.play==='') && 
+                    <div>
                     <p style={styles.para}>SONGS</p>
                     <p style={styles.para2} >song1</p>
                     <p style={styles.active}>song2</p>
@@ -30,7 +29,8 @@ class MusicMenu extends React.Component{
                     <p style={styles.para2}>Back</p>
                     </div>
                     }
-                    {(this.props.activeSong==='song3' && this.props.activeMenu==='music' && this.props.play==='') && <div>
+                    {(this.props.activeSong==='song3' && this.props.activeMenu==='music' && this.props.play==='') && 
+                    <div>
                     <p style={styles.para}>SONGS</p>
                     <p style={styles.para2} >song1</p>
                     <p style={styles.para2}>song2</p>
@@ -39,7 +39,8 @@ class MusicMenu extends React.Component{
                     <p style={styles.para2}>Back</p>
                     </div>
                     }
-                    {(this.props.activeSong==='song4' && this.props.activeMenu==='music' && this.props.play==='') && <div>
+                    {(this.props.activeSong==='song4' && this.props.activeMenu==='music' && this.props.play==='') && 
+                    <div>
                     <p style={styles.para}>SONGS</p>
                     <p style={styles.para2} >song1</p>
                     <p style={styles.para2}>song2</p>
@@ -48,7 +49,8 @@ class MusicMenu extends React.Component{
                     <p style={styles.para2}>Back</p>
                     </div>
                     }
-                    {(this.props.activeSong==='back' && this.props.activeMenu==='music' && this.props.play==='') && <div>
+                    {(this.props.activeSong==='back' && this.props.activeMenu==='music' && this.props.play==='') && 
+                    <div>
                     <p style={styles.para}>SONGS</p>
                     <p style={styles.para2} >song1</p>
                     <p style={styles.para2}>song2</p>
@@ -57,20 +59,28 @@ class MusicMenu extends React.Component{
                     <p style={styles.active}>Back</p>
                     </div>
                     }
-                    {(this.props.play==='song1' && this.props.activeSong==='song1' ) && <div>
-                    <p style={styles.para}>SONG1</p>
+                    {(this.props.play==='song1' && this.props.activeSong==='song1' ) && <div style={styles.container}>
+                    <p style={styles.para}>Pachtaoge</p>
+                    <img style={styles.img} alt="song-img" src="arijit.jpg"/>
+                    <audio style={styles.audio} src="./songs/Pachtaoge.mp3" controls loop></audio>
                     </div>
                     } 
-                    {(this.props.play==='song2' && this.props.activeSong==='song2') && <div>
-                    <p style={styles.para}>SONG2</p>
+                    {(this.props.play==='song2' && this.props.activeSong==='song2') && <div style={styles.container}>
+                    <p style={styles.para}>Nikle Currant</p>
+                    <img style={styles.img} alt="song-img" src="neha.jpg "/>
+                    <audio style={styles.audio} src="./songs/Nikle currant.mp3" controls loop></audio>
                     </div>
                     }  
-                    {(this.props.play==='song3' && this.props.activeSong==='song3') && <div>
-                    <p style={styles.para}>SONG3</p>
+                    {(this.props.play==='song3' && this.props.activeSong==='song3') && <div style={styles.container}>
+                    <p style={styles.para}>Tere Te</p>
+                    <img style={styles.img} alt="song-img" src="guru.jpg"/>
+                    <audio style={styles.audio} src="./songs/Tere te.mp3 " controls loop></audio>
                     </div>
                     } 
-                    {(this.props.play==='song4' && this.props.activeSong==='song4') && <div>
-                    <p style={styles.para}>SONG4</p>
+                    {(this.props.play==='song4' && this.props.activeSong==='song4') && <div style={styles.container}>
+                    <p style={styles.para}>Glorious Gallan</p>
+                    <img style={styles.img} alt="song-img" src="diljit.jpg "/>
+                    <audio style={styles.audio} src="./songs/Glorious gallan.mp3" controls loop></audio>
                     </div>
                     } 
                     
@@ -86,20 +96,43 @@ const styles={
     menu:{
         width:'inherit',
         height:'inherit',
-        paddingTop:2,
-        fontSize:15,
+        //paddingTop:2,
+        fontSize:18,
         fontWeight:400,
-        paddingLeft:10
+        //paddingLeft:10
     },
     para:{
         textAlign:'center'
     },
     para2:{
-        fontSize:15
+        fontSize:16,
+        paddingLeft:10
     },
     active:{
-        fontSize:15,
-        backgroundColor:'lightgrey'
+        fontSize:16,
+        backgroundColor:'lightgrey',
+        paddingLeft:10
+    },
+    audio:
+    {
+        width:250,
+        height:22,
+        backgroundColor:'transparent',
+        border:'none',
+        outline:'none'
+    },
+    img:{
+        
+        width:160,
+        height:120,
+        paddingLeft:22
+    },
+    container:
+    {
+        width:213.59,
+        height:220,
+        backgroundColor:' #f5f1f1',
+        overflow:'hidden'
     }
 
 }
